e95ff772bd362073f785d70037863799b418845d
Rajat Gupta <37516416+Rajat-Gupta-11605708@users.noreply.github.com>
2018-04-30 22:57:06 +0530

    Update lseek.c

--- lseek.c
+++ lseek.c
@@ -10,7 +10,7 @@ int main()
     n=open("file1.txt",O_RDWR);
     read(n,buffer,5); //reads first 5 characters
     write(1,buffer,5);
-    off = lseek(n,5,SEEK_SET); //pointer will be at 10th location from the beginning
+    off = lseek(n,5,SEEK_SET); //pointer will be at 5th location from the beginning
     printf("\nCurrent position is %d\n",off); // to print the position of the pointer
     read(n,buffer,4);
     write(1,buffer,4);
